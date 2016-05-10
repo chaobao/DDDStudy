9ea1d058fc06335cf8f295ae0d9db8caa7146312
chaobao <chaobao465@sina.com>
2016-05-11 01:13:52 +0800

    添加消息队列demo

---
+++ RabbitMq.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMq.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "yy";
+            factory.Password = "hello!";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" set {0}", message);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
